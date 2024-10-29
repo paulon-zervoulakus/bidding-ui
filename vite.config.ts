e552d777338e8a24277b60a2be9274ca4ba4b437
paulon-zervoulakus <paulon.zervoulakus@gmail.com>
2024-10-29 19:19:03 +0800

    vite compilation logic

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  const apiUrl = process.env.MODE === 'development' ?
-    (process.env.VITE_DEV_API_URL || 'http://localhost:8000') :
-    (process.env.VITE_PROD_API_URL || 'http://localhost:8000');
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+  const isDevelopment = mode === 'development';
   return {
     plugins: [react()],
     define: {
-      __API_URL__: JSON.stringify(apiUrl), // Inject the API URL as a global variable
+      __API_URL__: isDevelopment
+        ? JSON.stringify(env.VITE_DEV_API_URL || 'http://localhost:8081')
+        : JSON.stringify(env.VITE_PROD_API_URL || 'http://localhost:8082'),
     },
   };
 });
